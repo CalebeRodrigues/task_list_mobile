d13c5f6ce763c5b42cbeec424741983fe36b9772
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-01-30 21:27:03 -0300

    App bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new ListaTarefas());
+}
+
+class ListaTarefas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Lista de tarefas"),
+        ),
+        body: Column(),
+      ),
+    );
+  }
 }
